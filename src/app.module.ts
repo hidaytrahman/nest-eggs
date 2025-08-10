baace51a128c96cc89f5abfb1101fa4eaca7c435
hidaytrahman <hidaytrahman@gmail.com>
2025-08-10 18:02:20 +0530

    chore: package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,38 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserController } from './controllers/user.controller';
-import { UserService } from './services/user.service';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
-import { HttpExceptionFilter } from './exceptions/http-exception.filter';
-import { RolesGuard } from './guards/roles.guard';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+
+import { APP_FILTER, APP_GUARD } from "@nestjs/core";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserController } from "./controllers/user.controller";
+import { UserService } from "./services/user.service";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
+import { HttpExceptionFilter } from "./exceptions/http-exception.filter";
+import { RolesGuard } from "./guards/roles.guard";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { AuthService } from "./auth/auth.service";
+import { AuthController } from "./auth/auth.controller";
+import { CatsModule } from "./features/cats.module";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    CatsModule,
+    MongooseModule.forRoot(
+      // Replace <db_password> with your actual database password
+      // Ensure you have the correct connection string for your MongoDB instance
+      // get from environment variable or config file
+      "mongodb+srv://hidaytrahman:5qCUa79xCVA4e7PE@nest-eggs.v2c9wgi.mongodb.net/?retryWrites=true&w=majority&appName=nest-eggs",
+    ),
+  ],
   controllers: [AppController, UserController, AuthController],
   providers: [
     AppService,
     UserService,
     AuthService,
-    
+
     /* Global-scoped filters are used across the whole application, 
     for every controller and every route handler. 
     In terms of dependency injection, global filters registered from 
@@ -40,6 +53,6 @@ import { AuthController } from './auth/auth.controller';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes("*");
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './exceptions/http-exception.filter';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./exceptions/http-exception.filter";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +13,7 @@ async function bootstrap() {
 
 bootstrap()
   .then(() => {
-    console.log('Bootstrap done');
+    console.log("Bootstrap done");
   })
   .catch((err) => {
     console.error(err);
